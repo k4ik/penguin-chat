039375d62965195f28abdee597b36633060fbd9d
k4ik <kaikleal1234@gmail.com>
2024-08-04 11:13:05 -0300

    feat: change assets dir

--- index.php
+++ index.php
@@ -3,15 +3,15 @@ $path = $_SERVER["REQUEST_URI"];
 
 switch($path) {
     case "/":
-        include "resources/views/welcome/home.php";
+        include "resources/views/welcome/home.html";
         break;
     case "/login":
-        include "resources/views/auth/login.php";
+        include "resources/views/auth/login.html";
         break;
     case "/signup":
-        include "resources/views/auth/signup.php";
+        include "resources/views/auth/signup.html";
         break;
     default:
-        include "resources/views/error/404.php";
+        include "resources/views/error/404.html";
         break;
 }
