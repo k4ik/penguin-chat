c036be8eba43e8a528a81e437715794909729770
k4ik <kaikleal1234@gmail.com>
2024-08-04 11:47:48 -0300

    feat: db connection

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+$host = $_ENV["DB_HOST"];
+$pass = $_ENV["DB_PASS"];
+$username = $_ENV["DB_USER"];
+$dbname = $_ENV["DB_NAME"];
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
