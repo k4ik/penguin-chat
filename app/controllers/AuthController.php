964bc166c9a0c9ca0a1643767cce95390f08e53d
k4ik <kaikleal1234@gmail.com>
2024-08-17 11:45:06 -0300

    fix: controller

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,55 +1,32 @@
 <?php
 namespace Controller;
-include "../../config/database.php";
 
 class AuthController
 {
+    private $conn;
+
     public function __construct($conn)
     {
         $this->conn = $conn;
     }
 
-    public function login(string $username, string $password): void
+    public function auth(): void
     {
-        $username = $_POST["username"];
-        $password = $_POST["password"];
+        $nickname = $_POST["nickname"];
 
-        if(empty($username) || empty($password)) {
-            echo json_encode(["error" => "Preencha os campos"]);
+        if(empty($nickname)){
             return;
         }
 
-        $stmt = $conn->prepare('SELECT * FROM users WHERE username = :username AND passoword = :password');
-        $stmt->execute(array('username => $username', 'password' => $password));
+        $stmt = $this->conn->prepare('SELECT COUNT(*) FROM users WHERE nickname = :nickname');
+        $stmt->execute(['nickname' => $nickname]);
 
-        if ($stmt->rowCount() == 1) {
-            header("Location: /room");
-        }
-    }
-
-    public function signup(string $username, string $email, string $password): void
-    {
-        $username = $_POST["username"];
-        $email = $_POST["email"];
-        $password = $_POST["password"];
-
-        if(empty($username) || empty($email) || empty($password)) {
-            echo json_encode(["error" => "Preencha os campos"]);
-            return;
+        if($stmt->fetchColumn() == 0){
+            $stmt = $this->conn->prepare('INSERT INTO users(nickname) VALUES(:nickname)');
+            $stmt->execute(['nickname' => $nickname]);
         }
 
-        $stmt = $conn->prepare('SELECT * FROM users WHERE username = :username');
-        $stmt->execute(array('username => $username'));
-
-        $count = $stmt->rowCount();
-        if ($count == 1) {
-            echo json_encode(["error" => "Já existe um usuário com essas credenciais"]);
-            return;
-        }
-
-        $stmt = $conn->prepare('INSERT INTO users(name, email, password) VALUES(:username, :email, :password)');
-        $stmt->execute(array('username => $username', 'email => $email', 'password => $password'));
-
-        header('Location: /room');
+        header('Location: /chat');
+        exit();
     }
 }
